135ce82998551d55bb9ecb336b9231de4d2f95eb
Justyna Konieczna <justyna.bednarko@gmail.com>
2025-10-27 12:09:57 +0100

    add exercise with asks the user to guess a secret number

---
+++ test.py
@@ -0,0 +1,15 @@
+# TODO 1: Write a program that asks the user to guess a secret number
+
+secretNumber = 5
+yourNumber = 0
+
+while yourNumber != secretNumber:
+    yourNumber = int(input("Podaj liczbę: "))
+
+    if (yourNumber > secretNumber):
+        print("Twoja liczba jest większa od liczby szukanej")
+    elif (yourNumber < secretNumber):
+        print("Twoja liczba jest mniejsza od liczby szukanej")
+    else:
+        print("Wygrałeś")
+
