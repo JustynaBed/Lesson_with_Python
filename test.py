05045b356d889eb4420ad0ccb4a1924372a8eabc
Justyna Konieczna <justyna.bednarko@gmail.com>
2025-10-27 20:48:57 +0100

    Todo 5- Print all numbers from 1 to 100 that are divisible by 3 but not by 5

--- test.py
+++ test.py
@@ -39,12 +39,37 @@
 # for i in range(1, userNumberN + 1):
 #    print(i)
 
-# TODO 4: Calculate the sum of all numbers from 1 to 100 using a loop.
+# # TODO 4: Calculate the sum of all numbers from 1 to 100 using a loop.
+#
+# i =  1
+# result = 0
+#
+# while i <= 100:
+#     result += i
+#     print(i, result)
+#     i += 1
 
-i =  1
-result = 0
+# TODO 5: Print all numbers from 1 to 100 that are divisible by 3 but not by 5.
 
-while i <= 100:
-    result += i
-    print(i, result)
-    i += 1
+# version 1
+# i = 1
+# for i in range(1, 101):
+#     if i % 3 == 0 and i % 5 != 0:
+#         print(i)
+
+# version 2
+# i = 1
+# startNumber = 0
+# endNumber = 100
+# for i in range(startNumber, endNumber + 1):
+#     if i % 3 == 0 and i % 5 != 0:
+#         print(i)
+
+# version 3
+# i = 1
+# endNumber = 100
+#
+# while i <= endNumber:
+#     if i % 3 == 0 and i % 5 != 0:
+#         print(i)
+#     i += 1
